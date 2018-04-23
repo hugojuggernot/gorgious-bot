fa8fa4611b66b787f84495ef487d9e98a48de74e
hugojuggernot <hugo@youpla.vom>
2018-04-23 10:33:15 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,222 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', () => { bot.user.setGame('Victimise des daronnes') })
+
+client.login(process.env.BOT_TOKEN);
+
+bot.on('message', function (message) {
+
+  if (message.content === 'ping') {
+
+    message.channel.send('pong');
+
+  } else if (message.content === 'Ping') {
+
+    message.channel.send('Pong');
+	
+  } else if (message.content === 'Oui') {
+
+    message.channel.send('stisti https://image.jimcdn.com/app/cms/image/transf/dimension=830x10000:format=jpg/path/s5dde8bff85c81b2f/image/i949c1d8be0bd52ce/version/1424476325/ouistiti.jpg');
+
+  } else if (message.content === 'oui') {
+
+    message.channel.send('stisti https://image.jimcdn.com/app/cms/image/transf/dimension=830x10000:format=jpg/path/s5dde8bff85c81b2f/image/i949c1d8be0bd52ce/version/1424476325/ouistiti.jpg');
+
+  } else if (message.content === 'Intro') {
+
+    message.channel.send('https://www.youtube.com/watch?v=6rdwJkWdDNU');
+
+    } else if (message.content === 'ala') {
+
+    message.channel.send('akbar');
+
+   } else if (message.content === 'Ah') {
+
+       message.channel.send('bon');
+
+   } else if (message.content === 'Ala') {
+
+       message.channel.send('akbar');
+
+   } else if (message.content === 'ah') {
+
+              message.channel.send('bon');
+
+   } else if (message.content === 'AH') {
+
+              message.channel.send('bon');
+
+   } else if (message.content === 'Aïe') {
+
+                message.channel.send('HITLER');
+
+   } else if (message.content === 'aïe') {
+
+                message.channel.send('HITLER');
+
+   } else if (message.content === 'heil') {
+
+                message.channel.send('HITLER');
+
+   } else if (message.content === 'HEIL') {
+
+              message.channel.send('HITLER');
+
+   } else if (message.content === 'ez') {
+
+              message.channel.send('noraj');
+
+   } else if (message.content === 'Ez') {
+
+              message.channel.send('noraj');
+
+   } else if (message.content === 'EZ') {
+
+               message.channel.send('noraj');
+
+   } else if (message.content === 'Tg') {
+
+               message.channel.send('T Moche');
+
+     } else if (message.content === 'TG') {
+
+                 message.channel.send('T Moche');
+
+     } else if (message.content === 'tg') {
+
+                message.channel.send('T Moche');
+
+       } else if (message.content === 'lol') {
+
+                 message.channel.send('Ptdr xD mdr ololol');
+
+        } else if (message.content === 'Lol') {
+
+                  message.channel.send('Ptdr xD mdr ololol');
+
+         } else if (message.content === 'LOL') {
+
+                  message.channel.send('Ptdr xD mdr ololol');
+
+          } else if (message.content === 'LoL') {
+
+                      message.channel.send('Ptdr xD mdr ololol');
+
+                      } else if (message.content === 'jsp') {
+
+                       message.channel.send('Tu sais pas miskine?')
+
+                      } else if (message.content === 'JSP') {
+
+                        message.channel.send('Tu sais pas miskine?')
+
+                          } else if (message.content === 'Jsp') {
+
+                            message.channel.send('Tu sais pas miskine?')
+
+                           } else if (message.content === 'ça va') {
+
+                             message.channel.send('Et toi ? https://thumbs.gfycat.com/BitterLeafyAyeaye-size_restricted.gif')
+
+                            } else if (message.content === 'ça va ?') {
+
+                           message.channel.send('Et toi ? https://thumbs.gfycat.com/BitterLeafyAyeaye-size_restricted.gif')
+
+                           } else if (message.content === 'cv ?') {
+
+                            message.channel.send('Et toi ? https://thumbs.gfycat.com/BitterLeafyAyeaye-size_restricted.gif')
+
+                         } else if (message.content === 'pd') {
+
+                           message.channel.send('Personne dont l"attirance sexuelle est vers les hommes chez les garçon et vers les femelles chez les truck https://ae01.alicdn.com/kf/HTB1xJg8gbsTMeJjy1zeq6AOCVXaR/2017-Nouvelle-Arriv-e-Plus-La-Taille-Ours-Griffe-Hommes-de-Coton-M-moires-Gay-Ours.jpg_640x640.jpg')
+
+                          } else if (message.content === 'PD') {
+
+                             message.channel.send('Personne dont l"attirance sexuelle est vers les hommes chez les garçon et vers les femelles chez les truck https://ae01.alicdn.com/kf/HTB1xJg8gbsTMeJjy1zeq6AOCVXaR/2017-Nouvelle-Arriv-e-Plus-La-Taille-Ours-Griffe-Hommes-de-Coton-M-moires-Gay-Ours.jpg_640x640.jpg')
+
+                           } else if (message.content === 'Pd') {
+
+                           message.channel.send('Personne dont l"attirance sexuelle est vers les hommes chez les garçon et vers les femelles chez les truck https://ae01.alicdn.com/kf/HTB1xJg8gbsTMeJjy1zeq6AOCVXaR/2017-Nouvelle-Arriv-e-Plus-La-Taille-Ours-Griffe-Hommes-de-Coton-M-moires-Gay-Ours.jpg_640x640.jpg')
+
+                            } else if (message.content === 'gg') {
+
+                             message.channel.send('wp https://media1.tenor.com/images/987caa6022545984fc0c643747430ada/tenor.gif')
+
+                            } else if (message.content === 'Gg') {
+
+                             message.channel.send('wp https://media1.tenor.com/images/987caa6022545984fc0c643747430ada/tenor.gif')
+
+                            } else if (message.content === 'GG') {
+
+                             message.channel.send('wp https://media1.tenor.com/images/987caa6022545984fc0c643747430ada/tenor.gif')
+
+                       } else if (message.content === 'sardines') {
+
+                      message.channel.send('https://www.youtube.com/watch?v=PA3P1-aSvKQ')
+
+                    } else if (message.content === 'olol') {
+
+                   message.channel.send('olol jsui fransé lé arab degajé')
+
+                   } else if (message.content === 'Olol') {
+
+                     message.channel.send('olol jsui fransé lé arab degajé')
+
+                   } else if (message.content === 'salut') {
+
+                   message.channel.send('C"esttttttt POLOK')
+
+                 } else if (message.content === 'Salut') {
+
+                message.channel.send('C"esttttttt POLOK')
+
+                } else if (message.content === 'Bonjour') {
+
+                 message.channel.send('Aurevoir http://m.memegen.com/rchgmc.jpg')
+
+                } else if (message.content === 'bonjour') {
+
+                 message.channel.send('Aurevoir http://m.memegen.com/rchgmc.jpg')
+
+                } else if (message.content === 'a+') {
+
+                  message.channel.send('dans le bus http://img.bfmtv.com/c/1000/600/cec/4d7d94f17ddc37153cdb6688610e1.jpg')
+
+                } else if (message.content === 'A+') {
+
+                    message.channel.send('dans le bus http://img.bfmtv.com/c/1000/600/cec/4d7d94f17ddc37153cdb6688610e1.jpg')
+
+                    } else if (message.content === 'issou') {
+
+                     message.channel.send('Mi él bañador https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn6tdZTrJ7fzXA6yMRmjdC9OJ85DV5FZrU6CjAHjB7D4BMl-pU')
+
+                    } else if (message.content === 'Issou') {
+
+                      message.channel.send('Mi él bañador https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn6tdZTrJ7fzXA6yMRmjdC9OJ85DV5FZrU6CjAHjB7D4BMl-pU')
+
+                     } else if (message.content === 'Jul akbar') {
+
+                     message.channel.send('https://risibank.fr/cache/stickers/d376/37637-full.png')
+
+                     } else if (message.content === 'jul akbar') {
+
+                       message.channel.send('https://risibank.fr/cache/stickers/d376/37637-full.png')
+					   
+					   } else if (message.content === 'non') {
+
+                       message.channel.send('bah si miskine https://risibank.fr/cache/stickers/d559/55948-full.gif')
+					   
+					  } else if (message.content === 'Non') {
+
+                       message.channel.send('bah si miskine https://risibank.fr/cache/stickers/d559/55948-full.gif')
+					   
+					   } else if (message.content === 'NON') {
+
+                       message.channel.send('bah si miskine https://risibank.fr/cache/stickers/d559/55948-full.gif')
+
+
+					   
+}
+});
+
